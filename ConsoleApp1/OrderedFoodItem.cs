14cb7c978a4d59b6603d31296a7ec5c49889bc28
jee-eee <jiaen0936@gmail.com>
2026-01-29 13:46:45 +0800

    classes update

---
+++ ConsoleApp1/OrderedFoodItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class OrderedFoodItem
+    {
+        public FoodItem FoodItem { get; set; }
+
+        public int QtyOrdered { get; set; }
+
+        public double subTotal { get; set; }
+
+        public OrderedFoodItem() { }
+
+        public OrderedFoodItem(FoodItem foodItem, int qtyOrdered)
+        {
+            FoodItem = foodItem;
+            QtyOrdered = qtyOrdered;
+            subTotal = 0;
+        }
+
+        public double CalculateSubTotal()
+        {
+            subTotal = FoodItem.itemPrice * QtyOrdered;
+            return subTotal;
+        }
+
+        public override string ToString()
+        {
+            return "Item: " + FoodItem.ItemName + "  Qty: " + QtyOrdered + "  Price: $" + FoodItem.itemPrice + "  SubTotal: $" + subTotal;
+        }
+    }
+}
